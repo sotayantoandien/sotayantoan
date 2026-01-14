648a7fde5b0dfb673410578ed5b51702d489a48a
sotayantoandien <sotayantoandien@gmail.com>
2026-01-14 09:34:23 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,13 @@
+// js/config.js
+const CONFIG = {
+    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbz6K4E8o4934WuV1dY5fqi1YIzrVgyBMEzVlg0LVM4REBXMDkM4rdiZQ9BXd2e-bYGNmw/exec",
+    FIREBASE: {
+        apiKey: "AIzaSyCdT1WiSZUm1faQw2joWL8t3D360ydQO8U",
+        authDomain: "sotayantoan-dae90.firebaseapp.com",
+        projectId: "sotayantoan-dae90",
+        storageBucket: "sotayantoan-dae90.firebasestorage.app",
+        messagingSenderId: "1025561475254",
+        appId: "1:1025561475254:web:04e9b0306559208d2e0e17",
+        measurementId: "G-BL7ZM4QBW7"
+    }
+};
